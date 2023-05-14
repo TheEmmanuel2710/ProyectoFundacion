d32889282cbd34bb0570a799c01292b4cb1015ee
TheEmmanuel2710 <esgonzalez430@misena.edu.co>
2023-05-14 09:47:07 -0500

    Redireccionamiento de las vista y instalacion de node.js

--- GestionFundacion/appGestionFundacion/static/js/app.js
+++ GestionFundacion/appGestionFundacion/static/js/app.js
@@ -1,26 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const rutaFotos = '../media/fotos'; // Ruta de la carpeta fotos 
+const rutaFotos = '../media/fotos';
 var contenedorImagenes = document.getElementById('contenedor-fotos');
 fs.readdir(rutaFotos, (err, files) => {
   if (err) {
     console.error('Error al leer los archivos:', err);
     return;
   }
-  
-  // Filtrar solo los archivos con la extensión .png
-  const imagenesPNG = files.filter(file => path.extname(file).toLowerCase() === '.png');
-  
-  // Recorrer los nombres de las imágenes
+  const imagenesPNG = files.filter(file => path.extname(file).toLowerCase() === '.JPG');
   imagenesPNG.forEach(nombreImagen => {
-    // Crear la etiqueta <img> para cada imagen
     var imagen = document.createElement('img');
-    
-    // Establecer el atributo "src" con la ruta completa de la imagen
     imagen.src = rutaFotos + '/' + nombreImagen;
-    
-    // Agregar la etiqueta <img> al contenedor de imágenes
     contenedorImagenes.appendChild(imagen);
   });
 });
