8d24b8d498cc363360fec4958c9d7bee32c501cd
TheEmmanuel2710 <esgonzalez430@misena.edu.co>
2023-05-15 17:09:55 -0500

    Funcionalidad del carrusel

--- GestionFundacion/appGestionFundacion/static/js/app.js
+++ GestionFundacion/appGestionFundacion/static/js/app.js
@@ -1,17 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-
-const rutaFotos = '../media/fotos';
-var contenedorImagenes = document.getElementById('contenedor-fotos');
-fs.readdir(rutaFotos, (err, files) => {
-  if (err) {
-    console.error('Error al leer los archivos:', err);
-    return;
+document.addEventListener('DOMContentLoaded', function() {
+  let datos = "";
+  for (let index = 1; index < 139; index++) {
+    datos += "<div class='carousel-item'>";
+    datos += "<img src='../media/fotos/" + index +".JPG' class='d-block'></img>";
+    datos += "</div>";
   }
-  const imagenesPNG = files.filter(file => path.extname(file).toLowerCase() === '.JPG');
-  imagenesPNG.forEach(nombreImagen => {
-    var imagen = document.createElement('img');
-    imagen.src = rutaFotos + '/' + nombreImagen;
-    contenedorImagenes.appendChild(imagen);
-  });
+  document.querySelector('.carousel-inner').innerHTML = datos;
+  document.querySelector('.carousel-item').classList.add('active');
 });
